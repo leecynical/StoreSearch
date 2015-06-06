56bf9af2d85920838ca7b84c1eeacec6a286cd11
lijinzhao <leecynical@gmail.com>
2015-06-07 02:24:16 +0800

    construct tableView

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by 李金钊 on 15/6/6.
+//  Copyright (c) 2015年 lijinzhao. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
