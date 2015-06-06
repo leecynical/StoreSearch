71c099645f59da8176ab499806425bb1ebc9a1e2
lijinzhao <leecynical@gmail.com>
2015-06-06 19:13:18 +0800

    repush storesearch

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by 李金钊 on 15/6/6.
+//  Copyright (c) 2015年 lijinzhao. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
